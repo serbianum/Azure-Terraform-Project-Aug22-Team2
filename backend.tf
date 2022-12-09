eaea9f95e6896fa0b64e26ad9e7dace34acad8ca
serbianum <111622811+serbianum@users.noreply.github.com>
2022-12-09 11:13:02 -0800

    backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,14 @@
-
-    backend "azurerm" {
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
+  }
+  backend "azurerm" {
         resource_group_name  = "cloud-shell-storage-westus"
         storage_account_name = "wordpressbackend822"
         container_name       = "tfstate"
         key                  = "enter key here"
     }
-
 }
