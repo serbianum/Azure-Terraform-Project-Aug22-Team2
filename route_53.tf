73e814283a3f1213c05529b3bc3001ee14f9eaa9
serbianum <111622811+serbianum@users.noreply.github.com>
2022-12-09 07:58:24 -0800

    Create route_53.tf

---
+++ route_53.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "eu-west-1"
+    access_key=#add you access key
+    secret_key=#added secret key
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = "Z08323833Z5X1YWJQRPX"
+  name    = "team2.cloudjourneys.net"
+  type    = "A"
+  ttl     = 300
+  records = [azurerm_lb.wordpress.id]
+}
