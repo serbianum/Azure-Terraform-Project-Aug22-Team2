2a6df23faf66469b5ac3eadce48599363b7303b2
serbianum <111622811+serbianum@users.noreply.github.com>
2022-12-09 20:19:29 -0800

    edited

--- Mysql.tf
+++ Mysql.tf
@@ -1,36 +1,44 @@
-resource "azurerm_lb" "wordpress" {
-  name                = "wordpress-lb"
-  location            = var.location
+# Create MySQL Server
+resource "azurerm_mysql_server" "wordpress" {
   resource_group_name = azurerm_resource_group.wordpress.name
+  name                = "team2-db-server-wordpress"   #"${var.db_server_name}-${(random_string.fqdn.result)}"
+  location            = azurerm_resource_group.wordpress.location
+  version             = "5.7"
 
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.wordpress.id
-  }
+  administrator_login          = var.database_admin_login
+  administrator_login_password = var.database_admin_password
 
-  tags = var.tags
+  sku_name                     = "GP_Gen5_4"
+  storage_mb                   = "102400"
+  auto_grow_enabled            = false
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
+
+  infrastructure_encryption_enabled = false
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = false
+  #ssl_minimal_tls_version_enforced = "TLS1_2"
 }
 
-resource "azurerm_lb_backend_address_pool" "bpepool" {
-  loadbalancer_id = azurerm_lb.wordpress.id
-  name            = "BackEndAddressPool"
-}
-
-resource "azurerm_lb_probe" "wordpress" {
+# Create MySql DataBase
+resource "azurerm_mysql_database" "wordpress" {
+  name                = var.dbname
   resource_group_name = azurerm_resource_group.wordpress.name
-  loadbalancer_id     = azurerm_lb.wordpress.id
-  name                = "ssh-running-probe"
-  port                = var.application_port
+  server_name         = azurerm_mysql_server.wordpress.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
 }
 
-resource "azurerm_lb_rule" "lbnatrule" {
-  resource_group_name            = azurerm_resource_group.wordpress.name
-  loadbalancer_id                = azurerm_lb.wordpress.id
-  name                           = "http"
-  protocol                       = "Tcp"
-  frontend_port                  = var.application_port
-  backend_port                   = var.application_port
-  backend_address_pool_id        = azurerm_lb_backend_address_pool.bpepool.id
-  frontend_ip_configuration_name = "PublicIPAddress"
-  probe_id                       = azurerm_lb_probe.wordpress.id
+# Config MySQL Server Firewall Rule
+resource "azurerm_mysql_firewall_rule" "wordpress" {
+  name                = "wordpress-mysql-firewall-rule"
+  resource_group_name = azurerm_resource_group.wordpress.name
+  server_name         = azurerm_mysql_server.wordpress.name
+  start_ip_address    = azurerm_public_ip.wordpress.ip_address
+  end_ip_address      = azurerm_public_ip.wordpress.ip_address
+}
+
+data "azurerm_mysql_server" "wordpress" {
+  name                = azurerm_mysql_server.wordpress.name
+  resource_group_name = azurerm_resource_group.wordpress.name
 }
--- route_53.tf
+++ route_53.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-    region = "eu-west-1"
+    region = "us-east-2"
     access_key=#add you access key
     secret_key=#add your secret key
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = "Z08323833Z5X1YWJQRPX"
-  name    = "team2.cloudjourneys.net"
+  zone_id = #add zone ID
+  name    = #add name
   type    = "A"
   ttl     = 300
   records = [azurerm_public_ip.wordpress.ip_address]
